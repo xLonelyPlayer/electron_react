f462ad831ef32663ca4b32e0a5b0f7eabd5564e3
Augusto Prodohl <51026238+xLonelyPlayer@users.noreply.github.com>
2022-07-24 22:15:02 -0300

    Implemented primereact and navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Navbar } from './components/Components'
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Só os pika na cabeça
         </p>
         <a
           className="App-link"
---
+++ src/components/Components.js
@@ -0,0 +1,3 @@
+import Navbar from './Menubar/Menubar'
+
+export { Navbar }
